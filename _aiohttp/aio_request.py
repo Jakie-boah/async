12c68aefa202be9462ed0493e99e46d0cff78c4b
slonn <vanogalen@yandex.ru>
2024-11-04 14:50:04 +0300

    закончил создание таблиц и данных в них

--- _aiohttp/aio_request.py
+++ _aiohttp/aio_request.py
@@ -7,15 +7,15 @@ from loguru import logger
 @async_timed()
 async def main():
     async with aiohttp.ClientSession() as session:
-        url = 'http://example.com'
+        url = "http://example.com"
         status = await fetch_status(session, url)
-        print(f'Состояние для {url} было равно {status}')
+        print(f"Состояние для {url} было равно {status}")
 
 
 @async_timed()
 async def main_advanced():
     async with aiohttp.ClientSession() as session:
-        urls = ['http://example.com' for _ in range(1000)]
+        urls = ["http://example.com" for _ in range(1000)]
         requests = [fetch_status(session, url) for url in urls]
         status_codes = await asyncio.gather(*requests)
         print(status_codes)
@@ -24,7 +24,7 @@ async def main_advanced():
 @async_timed()
 async def gather_with_exception():
     async with aiohttp.ClientSession() as session:
-        urls = ['http://example.com', 'python://example.com']
+        urls = ["http://example.com", "python://example.com"]
         tasks = [fetch_status(session, url) for url in urls]
 
         results = await asyncio.gather(*tasks, return_exceptions=True)
@@ -41,9 +41,9 @@ async def gather_with_exception():
 async def as_complete_usage():
     async with aiohttp.ClientSession() as session:
         fetchers = [
-            fetch_status(session, 'http://example.com', 1),
-            fetch_status(session, 'http://example.com', 1),
-            fetch_status(session, 'http://example.com', 10)
+            fetch_status(session, "http://example.com", 1),
+            fetch_status(session, "http://example.com", 1),
+            fetch_status(session, "http://example.com", 10),
         ]
         for finished_task in asyncio.as_completed(fetchers):
             print(await finished_task)
@@ -53,9 +53,9 @@ async def as_complete_usage():
 async def as_completed_usage_timeout():
     async with aiohttp.ClientSession() as session:
         fetchers = [
-            fetch_status(session, 'http://example.com', 1),
-            fetch_status(session, 'http://example.com', 10),
-            fetch_status(session, 'http://example.com', 10)
+            fetch_status(session, "http://example.com", 1),
+            fetch_status(session, "http://example.com", 10),
+            fetch_status(session, "http://example.com", 10),
         ]
         for done_task in asyncio.as_completed(fetchers, timeout=2):
             try:
@@ -71,15 +71,30 @@ async def as_completed_usage_timeout():
 async def wait():
     async with aiohttp.ClientSession() as session:
         fetchers = [
-            asyncio.create_task(fetch_status(session, 'http://example.com', )),
-            asyncio.create_task(fetch_status(session, 'http://example.com', )),
-            asyncio.create_task(fetch_status(session, 'http://example.com', )),
+            asyncio.create_task(
+                fetch_status(
+                    session,
+                    "http://example.com",
+                )
+            ),
+            asyncio.create_task(
+                fetch_status(
+                    session,
+                    "http://example.com",
+                )
+            ),
+            asyncio.create_task(
+                fetch_status(
+                    session,
+                    "http://example.com",
+                )
+            ),
         ]
 
         done, pending = await asyncio.wait(fetchers)
 
-        logger.info(f'Число завершившихся задач: {len(done)}')
-        logger.info(f'Число ожидающих задач: {len(pending)}')
+        logger.info(f"Число завершившихся задач: {len(done)}")
+        logger.info(f"Число ожидающих задач: {len(pending)}")
 
         for done_task in done:
             logger.info(await done_task)
@@ -89,14 +104,24 @@ async def wait():
 async def wait_error_handling():
     async with aiohttp.ClientSession() as session:
         fetchers = [
-            asyncio.create_task(fetch_status(session, 'http://example.com', )),
-            asyncio.create_task(fetch_status(session, 'python://example.com', )),
+            asyncio.create_task(
+                fetch_status(
+                    session,
+                    "http://example.com",
+                )
+            ),
+            asyncio.create_task(
+                fetch_status(
+                    session,
+                    "python://example.com",
+                )
+            ),
         ]
 
         done, pending = await asyncio.wait(fetchers)
 
-        logger.info(f'Число завершившихся задач: {len(done)}')
-        logger.info(f'Число ожидающих задач: {len(pending)}')
+        logger.info(f"Число завершившихся задач: {len(done)}")
+        logger.info(f"Число ожидающих задач: {len(pending)}")
 
         for done_task in done:
             if done_task.exception() is None:
@@ -108,17 +133,22 @@ async def wait_error_handling():
 @async_timed()
 async def wait_error_handling_advanced():
     async with aiohttp.ClientSession() as session:
-        fetchers = \
-            [
-                asyncio.create_task(fetch_status(session, 'python://bad.com')),
-                asyncio.create_task(fetch_status(session, 'https://www.example.com', delay=3)),
-                asyncio.create_task(fetch_status(session, 'https://www.example.com', delay=3))
-            ]
+        fetchers = [
+            asyncio.create_task(fetch_status(session, "python://bad.com")),
+            asyncio.create_task(
+                fetch_status(session, "https://www.example.com", delay=3)
+            ),
+            asyncio.create_task(
+                fetch_status(session, "https://www.example.com", delay=3)
+            ),
+        ]
 
-        done, pending = await asyncio.wait(fetchers, return_when=asyncio.FIRST_EXCEPTION)
+        done, pending = await asyncio.wait(
+            fetchers, return_when=asyncio.FIRST_EXCEPTION
+        )
 
-        logger.info(f'Число завершившихся задач: {len(done)}')
-        logger.info(f'Число ожидающих задач: {len(pending)}')
+        logger.info(f"Число завершившихся задач: {len(done)}")
+        logger.info(f"Число ожидающих задач: {len(pending)}")
 
         for done_task in done:
             if done_task.exception() is None:
@@ -127,25 +157,27 @@ async def wait_error_handling_advanced():
                 logger.error(done_task.exception())
 
         for pending_task in pending:
-            logger.info('Останавливаю таск {}'.format(pending_task))
+            logger.info("Останавливаю таск {}".format(pending_task))
             pending_task.cancel()
 
 
 @async_timed()
 async def wait_first_completed():
     async with aiohttp.ClientSession() as session:
-        url = 'http://example.com'
+        url = "http://example.com"
 
         fetchers = [
             asyncio.create_task(fetch_status(session, url)),
             asyncio.create_task(fetch_status(session, url)),
-            asyncio.create_task(fetch_status(session, url))
+            asyncio.create_task(fetch_status(session, url)),
         ]
 
-        done, pending = await asyncio.wait(fetchers, return_when=asyncio.FIRST_COMPLETED)
+        done, pending = await asyncio.wait(
+            fetchers, return_when=asyncio.FIRST_COMPLETED
+        )
 
-        logger.info(f'Число завершившихся задач: {len(done)}')
-        logger.info(f'Число ожидающих задач: {len(pending)}')
+        logger.info(f"Число завершившихся задач: {len(done)}")
+        logger.info(f"Число ожидающих задач: {len(pending)}")
 
         for done_task in done:
             logger.info(await done_task)
@@ -154,16 +186,20 @@ async def wait_first_completed():
 @async_timed()
 async def wait_first_complete_loop():
     async with aiohttp.ClientSession() as session:
-        url = 'http://example.com'
-        pending = [asyncio.create_task(fetch_status(session, url)),
-                   asyncio.create_task(fetch_status(session, url)),
-                   asyncio.create_task(fetch_status(session, url))]
+        url = "http://example.com"
+        pending = [
+            asyncio.create_task(fetch_status(session, url)),
+            asyncio.create_task(fetch_status(session, url)),
+            asyncio.create_task(fetch_status(session, url)),
+        ]
 
         while pending:
-            done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
+            done, pending = await asyncio.wait(
+                pending, return_when=asyncio.FIRST_COMPLETED
+            )
 
-            logger.info(f'Число завершившихся задач: {len(done)}')
-            logger.info(f'Число ожидающих задач: {len(pending)}')
+            logger.info(f"Число завершившихся задач: {len(done)}")
+            logger.info(f"Число ожидающих задач: {len(pending)}")
 
             for done_task in done:
                 logger.info(await done_task)
--- _asyncpg/connection.py
+++ _asyncpg/connection.py
@@ -2,26 +2,36 @@ import asyncio
 import asyncpg
 from loguru import logger
 from _asyncpg.products import *
+from asyncpg import Record
+from typing import List, Tuple
+from random import sample
+
+
+def load_common_words() -> List[str]:
+    with open("common-words.txt") as f:
+        return f.readlines()
+
+
+def generate_brand_names(words: List[str]) -> List[Tuple[str,]]:
+    return [(words[index],) for index in sample(range(100), 100)]
+
+
+async def insert_brands(common_words, connection) -> int:
+    brands = generate_brand_names(common_words)
+    insert_brands = "INSERT INTO brand VALUES(DEFAULT, $1)"
+    return await connection.executemany(insert_brands, brands)
 
 
 async def main():
+    common_words = load_common_words()
     connection = await asyncpg.connect(
-        host='localhost', port=5432, database='postgres', user='postgres',
-        password='postgres'
+        host="localhost",
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="postgres",
     )
-    statements = [CREATE_BRAND_TABLE,
-                  CREATE_PRODUCT_TABLE,
-                  CREATE_PRODUCT_COLOR_TABLE,
-                  CREATE_PRODUCT_SIZE_TABLE,
-                  CREATE_SKU_TABLE,
-                  SIZE_INSERT,
-                  COLOR_INSERT]
-
-    logger.info('Создается база данных product...')
-    for statement in statements:
-        status = await connection.execute(statement)
-    logger.info(status)
-    logger.info('База данных product создана!')
+    await insert_brands(common_words, connection)
     await connection.close()
 
 
